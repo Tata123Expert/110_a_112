665e5c139d5f3c2d70cdeb1a21e1c3982bedab91
Tata123Expert <117285408+Tata123Expert@users.noreply.github.com>
2023-07-14 16:27:20 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,22 +1,47 @@
-/* https://teachablemachine.withgoogle.com/models/mzEtQNVC9/ */
+prediction1 = ""
+prediction2 = ""
 
+Webcam.set
+(
+  {
+    width:350,
+    height:300,
+    imageFormat:'png',
+    pngQuality:90
+  }
+);
 
+camera = document.getElementById("camera");
+Webcam.attach('#camera');
 
+function takeSnapshot()
+{
+    Webcam.snap
+    (
+      function(data_uri)
+      {
+        document.getElementById("result").innerHTML = '<img id="captured_image" src="'+data_uri+'"/>';
+      }
+    )
+}
 
+console.log('ml5 version:', ml5.version);
 
+classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/mzEtQNVC9/model.json', modelLoaded);
 
+function modelLoded()
+{
+    console.log('Modelo Carregado!')
+}
 
-
-
-
-
-function speak(){
+function speak()
+{
     var synth = window.speechSynthesis;
     speakData1 = "A primeira previsão é " + prediction1;
     speakData2 = "E a segunda previsão é " + prediction2;
     var utterThis = new SpeechSynthesisUtterance(speakData1 + speakData2);
     synth.speak(utterThis);
-  }
+}
   
   
     function check()
@@ -24,3 +49,5 @@ function speak(){
       img = document.getElementById('captured_image');
       classifier.classify(img, gotResult);
     }
+    
+/*https://teachablemachine.withgoogle.com/models/mzEtQNVC9/*/
